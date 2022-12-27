a9fb04ba0bc568ad93bbb9eccb443795fd20337c
Aleksey Krupskyi <108680778+LefanTwelve@users.noreply.github.com>
2022-12-27 18:48:14 +0200

    Update color-switcher. html/js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const COLOR_FREQUENCY = 1000;
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', onStartClick);
+
+function onStartClick(e) {
+  const { startBtn, stopBtn } = refs;
+  const intervID = setInterval(changeColor, COLOR_FREQUENCY);
+
+  changeColor();
+  toggleActiveBtn(stopBtn, startBtn);
+
+  stopBtn.addEventListener('click', onStopClick(intervID), { once: true });
+}
+
+function onStopClick(intervID) {
+  return function (e) {
+    const { startBtn, stopBtn } = refs;
+
+    clearInterval(intervID);
+    toggleActiveBtn(startBtn, stopBtn);
+  };
+}
+
+function changeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleActiveBtn(activeBtn, inactiveBtn) {
+  inactiveBtn.setAttribute('disabled', true);
+  activeBtn.removeAttribute('disabled');
+}
